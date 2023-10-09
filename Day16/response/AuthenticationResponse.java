6e33eca6c17c87ac9db63de6cbb39a7b25f809ae
Darsani K <119998605+DarsaniK@users.noreply.github.com>
2023-10-09 11:58:43 +0530

    Add files via upload

---
+++ Day16/response/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.example.plantpro.response;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationResponse {
+    private String token;
+    private Long id;
+}
